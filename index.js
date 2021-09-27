b42e006ce74b38103c91fb500f73416ee0cc0fb9
chihchun0718 <chihchun0718@gmail.com>
2021-09-27 03:33:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+
+// render footer year and copyright
+let year = new Date().getFullYear();
+const innerFooter = document.getElementById("year")
+innerFooter.innerHTML = `© ${year} Copyright Julie All reserved.`
